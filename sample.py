de5cb1a622c3718c8f47651459f17042f72baa80
ryota <r.enumura@icloud.com>
2021-11-14 00:35:44 +0900

    add samle.py

---
+++ sample.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+def add(a,b):
+    return a+b
+
+class TestAdd(unittest.TestCase):
+    def testaddfunc(self):
+        val1 = 2
+        val2 = 3
+        expected = 5
+        self.assertEqual(add(val1, val2),  expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
